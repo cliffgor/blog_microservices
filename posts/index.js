9f10496d4d3ec716481b60ce86eb420cf6dbaab3
cliffgor <clierozigy@gmail.com>
2020-05-23 05:01:46 +0300

    1. added an object to handle all posts that are to be posted 2. created a hundler to send all posts when a get request is initiated 3. created a hundler with a function that generates an id in form of a hexadecimal string when a new post is created

--- posts/index.js
+++ posts/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require ('crypto');
 
 const app = express();
+app.use(bodyParser.json());
 
+
+// Object to store all posts that are going to be posted 
+const posts = {}
+
+
+// handlers to send all posts that are created when a get request is intstantiated
 app.get('/posts', (req, res)   => {
-
+    res.send(posts);
 });
 
+// radnomly generate an id when someone creates a post
 app.post('/posts', (req, res) =>{
+    // this will give a ramndom id in hexadecimal form e.g (ldjbdklfgkhdfkdhfv)
+    const id = randomBytes(4).toString('hex');
+    
+    const { title } = req.body;
 
+    posts[id] = {
+        id, title
+    };
+
+
+    res.status(201).send(posts[id]);
 });
 
 
